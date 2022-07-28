4c4591accda4cfd3073e39f67b8c74f0f4a086c5
AlexeyVodopyan <pa_to_atm>
2022-07-28 23:41:43 +0300

    feat(docker): добавил файлы докера

---
+++ src/db.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+db_user = os.getenv("DB_USER", "vlp")
+db_pass = os.getenv("DB_PASS", "vlp")
+db_host = os.getenv("DB_HOST", "localhost")
+db_port = os.getenv("DB_PORT", "5432")
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/vlp"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={
+        "check_same_thread": False
+    }
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
