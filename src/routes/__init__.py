267a5925e3e73d46237876cab2e0547774c580c8
Alex <32038216+AlexeyVodopyan@users.noreply.github.com>
2022-08-02 00:05:55 +0300

    feat(queries): добавлены заготовки под будущие эндпоинты

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -8,7 +8,8 @@ main_router = APIRouter(prefix="/vlp", tags=["VLP"])
 
 
 @main_router.post("/calc", response_model=VlpCalcResponse)
-def my_profile(vlp_in: VlpCalcRequest, session=Depends(get_session)):
+def calc_vlp(vlp_in: VlpCalcRequest, session=Depends(get_session)):
+    """Расчёт VLP по исходным данным и сохранение в Базу"""
     init_data_id = save_init_data(session, vlp_in.dict())
 
     vlp_result = calc_vlp(**vlp_in.dict())
@@ -16,3 +17,21 @@ def my_profile(vlp_in: VlpCalcRequest, session=Depends(get_session)):
     save_vlp_data(session, vlp_result, init_data_id)
 
     return VlpCalcResponse.parse_obj(vlp_result)
+
+
+@main_router.get('/get_vlp')
+def get_vlp():
+    """Вернуть VLP по ID"""
+    pass
+
+
+@main_router.get('/get_vlp_id_by_depth')
+def get_vlp():
+    """Вернуть все VLP ID больше глубины скважины по возрастанию глубины"""
+    pass
+
+
+@main_router.get('/get_vlp')
+def get_vlp():
+    """Вернуть VLP ID только для скважин обводненностью > 90 % по убыванию"""
+    pass
