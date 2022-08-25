597f01ee5e7e57a70234e1629afa7c892443ae56
Alex <32038216+AlexeyVodopyan@users.noreply.github.com>
2022-08-25 17:44:47 +0300

    feat(it-school): добавил задание

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -1,10 +1,6 @@
-from src.calculations.vlp import calc_vlp as vlp_calculation
 from fastapi import APIRouter
 from src.models.models import VlpCalcRequest, VlpCalcResponse
 from src.routes.queries import save_init_data, save_vlp_data, get_check_well_data_exists, get_check_vlp_exists
-from src.db import get_session
-from hashlib import sha256
-import json
 
 main_router = APIRouter(prefix="/vlp", tags=["VLP"])
 
@@ -12,20 +8,6 @@ main_router = APIRouter(prefix="/vlp", tags=["VLP"])
 @main_router.post("/calc", response_model=VlpCalcResponse)
 def calc_vlp(vlp_in: VlpCalcRequest):
     """Расчёт VLP по исходным данным и сохранение в Базу"""
-    session = get_session()
-
-    init_data = vlp_in.dict()
-    well_data_hash = sha256(json.dumps(init_data).encode()).hexdigest()
-    well_data_id = get_check_well_data_exists(session, well_data_hash)
-
-    if not well_data_id:
-        save_init_data(session, init_data, well_data_hash)
-        well_data_id = well_data_hash
-
-    vlp_result = get_check_vlp_exists(session, well_data_id)
-
-    if not vlp_result:
-        vlp_result = vlp_calculation(**init_data)
-        save_vlp_data(session, vlp_result, well_data_id)
-
-    return VlpCalcResponse.parse_obj(vlp_result)
+    # функция считающая VLP
+    from src.calculations.vlp import calc_vlp as vlp_calculation  # noqa
+    pass
--- src/tables/models.py
+++ src/tables/models.py
@@ -1,37 +1,9 @@
-import uuid
-
-from sqlalchemy import Column, ForeignKey, JSON, Float, String
-from sqlalchemy.orm import relationship
-from sqlalchemy.dialects.postgresql import UUID
 from src.db import Base
 
 
 class WellData(Base):
     __tablename__ = "well_data"
 
-    id = Column(String, primary_key=True)
-    inclinometry = Column(JSON, comment="Инклинометрия")
-    d_cas = Column(Float, comment="Диаметр ЭК, м")
-    d_tub = Column(Float, comment="Диаметр НКТ, м")
-    h_tub = Column(Float, comment="Глубина спуска НКТ, м")
-    wct = Column(Float, comment="Обводненность, %")
-    rp = Column(Float, comment="Газовый фактор, м3/т")
-    gamma_oil = Column(Float, comment="Плотность нефти")
-    gamma_gas = Column(Float, comment="Плотность газа")
-    gamma_wat = Column(Float, comment="Плотность воды")
-    t_res = Column(Float, comment="Температура пласта, С")
-    p_wh = Column(Float, comment="Буферное давление, атм")
-    geo_grad = Column(Float, comment="Температурный градиент, С/100 м")
-    h_res = Column(Float, comment="Глубина верхних дыр перфорации, м")
-
-    vlp = relationship("VLP", back_populates="data")
-
 
 class VLP(Base):
     __tablename__ = "vlp"
-
-    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
-    vlp = Column(JSON, comment="VLP")
-    well_id = Column(String, ForeignKey("well_data.id"))
-
-    data = relationship("WellData", back_populates="vlp")
