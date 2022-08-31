ff9336361f60ce728a58a57deebe19a328b470d0
Marat <maratabdrahmanov@yandex.ru>
2022-08-31 07:52:32 +0300

    Сделана схема таблиц базы данных и генереций Сделан Endpoint для запроса к бд

--- src/calculations/vlp.py
+++ src/calculations/vlp.py
@@ -2,7 +2,7 @@ import numpy as np
 from src.calculations.well import calc_pwf
 
 
-async def calc_vlp(inclinometry: dict, casing: dict, tubing: dict, pvt: dict,
+def calc_vlp(inclinometry: dict, casing: dict, tubing: dict, pvt: dict,
              p_wh: float, geo_grad: float, h_res: float):
 
     q_liq = np.linspace(0.001, 400, 7)
@@ -12,7 +12,6 @@ async def calc_vlp(inclinometry: dict, casing: dict, tubing: dict, pvt: dict,
 
         p_wf[i] = calc_pwf(inclinometry, casing, tubing, pvt, p_wh, geo_grad,
                            h_res, ql)
-
     result = {"q_liq": q_liq.tolist(), "p_wf": p_wf.tolist()}
 
     return result
--- src/tables/models.py
+++ src/tables/models.py
@@ -1,9 +1,29 @@
 from src.db import Base
-
+from typing import List
+import sqlalchemy as sa
 
 class WellData(Base):
     __tablename__ = "well_data"
 
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    MD = sa.Column(sa.String,  comment="")
+    TVD = sa.Column(sa.String)
+    d_casing = sa.Column(sa.Float)
+    d_tubing = sa.Column(sa.Float)
+    h_mes_tubing = sa.Column(sa.Float)
+    wct = sa.Column(sa.Integer)
+    rp = sa.Column(sa.Integer)
+    gamma_oil = sa.Column(sa.Float)
+    gamma_gas = sa.Column(sa.Float)
+    gamma_wat = sa.Column(sa.Float)
+    t_res = sa.Column(sa.Integer)
+    p_wh = sa.Column(sa.Integer)
+    geo_grad = sa.Column(sa.Integer)
+    h_res = sa.Column(sa.Integer)
+
 
 class VLP(Base):
     __tablename__ = "vlp"
+    id = sa.Column(sa.Integer,primary_key=True, comment="id", autoincrement=True)
+    q_liq = sa.Column(sa.String,comment="Дебиты жидкости, м3/сут")
+    p_wf = sa.Column(sa.String, comment=' Забойное давление, атм')
